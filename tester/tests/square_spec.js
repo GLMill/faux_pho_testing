8cd621b7dbe3e988bcfd48926184603a0a6649e5
Huxley_Millard <gerogiamillardccm@gmail.com>
2019-04-18 13:39:06 +0100

    README: added images of the project

--- tester/tests/square_spec.js
+++ tester/tests/square_spec.js
@@ -1,16 +1,3 @@
-  function sum(a,b){
-    return a + b;
-  }
-
-
-  describe('sum', ()=>{
-    it('adds two numbers', ()=>{
-      expect(sum(2,2)).toBe(4);
-    })
-  })
-
-  // ====== end of dummy code
-
 
 describe('error', ()=>{
   it('throws an error if num isnt an integer', ()=>{
